abff32c75be9f164dcf6f69d4347ff054b130ba8
Péter <peter.mako99@gmail.com>
2021-09-24 17:23:33 +0200

    Recursive dependency cruising is now working

--- js/index.js
+++ js/index.js
@@ -41,22 +41,92 @@ function populateVersions(packageName, versions){
     }
 }
 
-function getDependencies(){
-    var packageName = document.getElementById("mySelect").value.split(" ");
-    var directDependencies = getPackage(packageName[0], packageName[1], "dependencies");
+function getDependencies(package){   
+    package.Name = package.Name.replace('/', '%2f');
+    package.Version = package.Version.replace('^', '');
+    var directDependencies = getPackage(package.Name, package.Version, "dependencies");
+    var returnDeps = [];
 
-    if (document.contains(document.getElementById("directDepList"))) {
+    if(typeof(directDependencies) == "undefined"){
+        return false;
+    }
+
+    const depVersions = Object.values(directDependencies);
+    var vCount = 0;
+
+    for(dep in directDependencies){
+        returnDeps.push({Name: dep, Version: depVersions[vCount]});
+        vCount++;
+    }
+
+    return returnDeps;
+}
+
+function getDependenciesTillDepth(depth=6){
+    var package = document.getElementById("mySelect").value.split(" ");
+    package = {Name: package[0], Version: package[1]};
+    var packages = [];
+    var dependencies = [];
+    var alrdyExists = false;
+    
+    for(let i=0; i<depth; i++){
+
+        if(i==0){
+            dependencies = dependencies.concat(populateDependencies(package, i));
+            packages.push(package.Name.replace('%2f','/'));
+        }else{
+            for(const dep of dependencies){
+                
+                if(dep.Depth == i-1){
+                    for(const pckg of packages){
+                        if(dep.Name == pckg){
+                            alrdyExists = true;
+                            break;
+                        }
+                    }
+                    if(!alrdyExists){
+                        package = {Name: dep.Name, Version: dep.Version};
+                        dependencies = dependencies.concat(populateDependencies(package, i));
+                        packages.push(package.Name.replace('%2f','/'));
+                    }else{
+                        alrdyExists = false;
+                    }
+                }
+            }
+        }
+          
+    }
+    console.log(dependencies);  
+    console.log(packages);
+    return dependencies;
+}
+
+function populateDependencies(package, depth){
+    var dependencies = [];
+    var currentDependencies = getDependencies(package);
+    var n = 0;
+    if (!currentDependencies){
+        return [];
+    }
+    for(const dep of currentDependencies){
+        dependencies.push({
+            Name: dep.Name, 
+            Version: dep.Version.replace('^',''), 
+            Depth: depth, 
+            Parent: package.Name.replace('%2f', '/'), 
+            ParentVersion: package.Version
+        });
+        n++;
+    }
+    return dependencies;
+}
+
+
+/*if (document.contains(document.getElementById("directDepList"))) {
         document.getElementById("directDepList").remove();
     }
 
     var myDiv = document.getElementById("directDependencies");
     var depList = document.createElement("ul");
     depList.setAttribute("id", "directDepList");
-    myDiv.appendChild(depList);
-
-    for(dep in directDependencies){
-        var listElement = document.createElement("li");
-        listElement.innerHTML = dep;
-        depList.appendChild(listElement)
-    }
-}
+    myDiv.appendChild(depList);*/
