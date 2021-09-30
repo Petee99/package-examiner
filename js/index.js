ccff7167b14dab67845a6016238842701d1d6d0e
Péter <peter.mako99@gmail.com>
2021-09-30 22:55:09 +0200

    Touch ups, angular dependencies still need work

--- js/index.js
+++ js/index.js
@@ -5,12 +5,20 @@ function getPackage(packageName, packageVersion="", requiredData=""){
     var registryUrl = "https://registry.npmjs.cf/";
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", registryUrl+packageName+"/"+packageVersion, false ); // false for synchronous request
-    xmlHttp.send( null );
+    
+    xmlHttp.onreadystatechange = () => {
+        if (xmlHttp.status === 404) {
+            alert("There's no such package!");
+        }
+    };
+    
+    xmlHttp.send( null );   
+
     if(requiredData.length>0){
         return JSON.parse(xmlHttp.responseText)[requiredData];
     }else{
         return JSON.parse(xmlHttp.responseText);
-    }   
+    }
 }
 
 /*
@@ -20,29 +28,27 @@ function getPackageData(event){
     event.preventDefault();
     var packageName = document.getElementsByName("pname")[0].value;
     var packageObject = getPackage(packageName);
-    populateVersions(packageName, packageObject.versions);
+    populateVersionDOM(packageName, packageObject.versions);
 }
 
 /*
 This function populates the dropdown list on the site with the versions of the given package
 */
-function populateVersions(packageName, versions){ 
-    if (document.contains(document.getElementById("mySelect"))) { //Checks if the list already exists, destroys it if it does
-        document.getElementById("mySelect").remove();
-    }
+function populateVersionDOM(packageName, versions){ 
+    
     var versionArray =[];
     var i = 0;
-    var myDiv = document.getElementById("myDiv");
-    var selectList = document.createElement("select");
-    selectList.setAttribute("id", "mySelect");
-    myDiv.appendChild(selectList);
+
+    var selectList = document.getElementById("versionSelect");
+    selectList.innerHTML="";
 
     for(version in versions){ //Populates the version array
         versionArray[i] = version;
         i++;
     }
 
-    for(i=versionArray.length-1; i>=0; i--){ //Since the versions we got back are in ascending order, the program iterates through the array from bottom to top
+    //Since the versions we got back are in ascending order, the program iterates through the array from bottom to top and appends it to the DOM
+    for(i=versionArray.length-1; i>=0; i--){ 
         var option = document.createElement("option");
         option.setAttribute("value", packageName+" "+versionArray[i]);
         option.text = versionArray[i];
@@ -55,7 +61,7 @@ This function returns all dependencies of the given package, if there's any
 */
 function getDependencies(package){   
     package.Name = package.Name.replace('/', '%2f');
-    package.Version = package.Version.replace('^', '');
+    package.Version = package.Version.replace('^','').replace('~','');
     var directDependencies = getPackage(package.Name, package.Version, "dependencies");
     var returnDeps = [];
 
@@ -83,7 +89,6 @@ function getDependenciesTillDepth(package, depth){
     
     var packages = [];
     var dependencies = [];
-    var alrdyExists = false;
     var i = 0;
 
     do{
@@ -91,9 +96,9 @@ function getDependenciesTillDepth(package, depth){
             dependencies = dependencies.concat(populateDependencies(package, i));
             packages.push({Name: package.Name.replace('%2f','/'), Version: package.Version, Level: i});
         }
-        else{ //The rest of the levels. The function checks for duplicate packagenames, so it won't search for dependencies of a package, when it's already been done  
-            for(const dep of dependencies){
-                console.log(dep);    
+        else{ //The rest of the levels. The function checks for duplicate packagenames, so it won't search for dependencies of a package, when it's already been done             
+            for(const dep of dependencies){  
+                var alrdyExists = false;
                 if(dep.Depth == i){
                     for(const pckg of packages){
                         if(dep.Name == pckg.Name){
@@ -102,42 +107,20 @@ function getDependenciesTillDepth(package, depth){
                         }
                     }
                     if(!alrdyExists){
-                        package = {Name: dep.Name, Version: dep.Version};
-                        dependencies = dependencies.concat(populateDependencies(package, i));
-                        packages.push({Name: package.Name.replace('%2f','/'), Version: package.Version, Level: dep.Depth});    
-                    }
-                    else{
-                        alrdyExists = false;
+                        package = {Name: dep.Name, Version: dep.Version};                        
+                        packages.push({Name: package.Name.replace('%2f','/'), Version: package.Version, Level: dep.Depth}); 
+                        
+                        if(i+1<=depth || depth==0){ //If the there are no more iterations, don't add new dependencies
+                            dependencies = dependencies.concat(populateDependencies(package, i)); 
+                        }   
                     }
                 }
             }
         }
         i++;
-    }while(depth==='' && dependencies[i]!==dependencies[i-1] || depth!=0 && i<depth);
-    
-    //This below does another iteration of the previous loop to add the packages on the new depth level,
-    //This needs to be deleted from here and the loop should be rewritten to work flawlessly
-    for(const dep of dependencies){
-        console.log(dep);    
-        if(dep.Depth == i){
-            for(const pckg of packages){
-                if(dep.Name == pckg.Name){
-                    alrdyExists = true;
-                    break;
-                }
-            }
-            if(!alrdyExists){
-                package = {Name: dep.Name, Version: dep.Version};
-                packages.push({Name: package.Name.replace('%2f','/'), Version: package.Version, Level: dep.Depth});    
-            }
-            else{
-                alrdyExists = false;
-            }
-        }
-    }
+    }while(depth==0 && dependencies[i]!==dependencies[i-1] || depth!=0 && i<=depth); //The loop will stop if it reaches the given depth level or there are no more dependencies
 
     dependencies.push(packages);
-    console.log(dependencies);
     return dependencies;
 }
 
@@ -147,20 +130,19 @@ This function returns a subarray for getDependenciesTillDepth() containing the d
 */
 function populateDependencies(package, depth){
     var dependencies = [];
+    console.log(package);
     var currentDependencies = getDependencies(package);
-    var n = 0;
     if (!currentDependencies){
         return [];
     }
     for(const dep of currentDependencies){ //Populates the dependencies array of the current package, also includes the current depth level
         dependencies.push({
             Name: dep.Name, 
-            Version: dep.Version.replace('^',''), 
+            Version: dep.Version.replace('^','').replace('~',''), 
             Depth: depth+1, 
             Parent: package.Name.replace('%2f', '/'), 
             ParentVersion: package.Version
         });
-        n++;
     }
     return dependencies;
 }
@@ -171,19 +153,20 @@ This function handles the form responsible for getting and graphing the dependen
 function drawDepGraph(event){
     event.preventDefault();
     var dDepth = document.getElementsByName("ddepth")[0].value;
-    var package = document.getElementById("mySelect").value.split(" ");
+    var package = document.getElementById("versionSelect").value.split(" ");
 
     var dependencies = getDependenciesTillDepth(package, dDepth);
     var packages = dependencies[dependencies.length-1];
     document.getElementById("dTitle").innerHTML=package[0];
     dependencies.pop(dependencies[dependencies.length-1]);
 
-    console.log(packages);
-
     if(document.getElementById("container").innerHTML != ""){
         document.getElementById("container").innerHTML = "";
     }
 
+    console.log(dependencies);
+
+    //Creates a new sigma.js instance, and configures it
     var s = new sigma({ 
         container: 'container',
         renderer: {
@@ -193,41 +176,48 @@ function drawDepGraph(event){
         settings: {
             defaultEdgeType: "arrow",
             minArrowSize: 10,
-            defaultLabelColor: '#ccc'
+            sideMargin: 5,
+            defaultLabelColor: '#ccc',
+            defaultNodeColor: '#2e946d',
         }
     });  
 
-    var n = 0;
-    var offset = 0;
-
+    // Create graph nodes from packages
     for (let i = 0; i < packages.length; i++) {
-        if(i>0 &&packages[i].Level != packages[i-1].Level){
-            offset = 0;
-        }
-        
         s.graph.addNode({
-            // Main attributes:
             id: packages[i].Name,
             label: packages[i].Name,
-            // Display attributes:
-            x: 0+offset,
-            y: 0+packages[i].Level/2,
-            size: 1,
-            color: '#2e946d'
+            x: Math.random(),
+            y: Math.random(),
+            size: 1
         })
-        offset+=0.1;
     }
 
+    // Create graph edges from dependencies
+    var n = 0;
     dependencies.forEach(dep => {
         s.graph.addEdge({
             id: 'edge_'+n,
-            // Reference extremities:
             source: dep.Parent,
             target: dep.Name,
             color: '#ccc'
         });
         n++;
     });
-
+    
+    // Starts the algorithm, and kills it once it's drawn, to save resources:
     s.startForceAtlas2();
+    window.setTimeout(function() {s.killForceAtlas2()}, 100);
+
+    /*
+        Következő lépés:
+
+        A függőségi gráf komplexitására kellene mondani valamit. 
+        Lehet nézni például: 
+            függőségek számára vonatkozóan eloszlást, 
+            gráf mélységét, 
+            mennyire fáról vagy hálóról van szó.
+
+    */
+
 }
