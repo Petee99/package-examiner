2c6620513589adad23f7b6bb53463f5bb323f065
Peter <70147953+Petee99@users.noreply.github.com>
2021-10-29 23:37:15 +0200

    fixed memory bug

--- src/scripts/graphing/calculateGraphData.js
+++ src/scripts/graphing/calculateGraphData.js
@@ -1,79 +1,54 @@
 const sigma = require("sigma");
 const colors = ['#2e946d','#F0A30A','#2980B9','#A20025','#FFAB91','yellow','blue','pink','#795548','#607D8B'];
 
-function calculateGraphData(packages, dependencies, drawGraph = true){
+function calculateGraphData(packages, dependencies, shouldDrawGraph = true){
     //Creates a new sigma.js instance, and configures it
-    
+
     if(drawGraph){
         document.getElementById("container").innerHTML="";
         document.getElementById("showReq").innerHTML ="";
     }
 
-    var s = new sigma({ 
-        container: 'container',
-        renderer: {
-          container: document.getElementById('container'),
-          type: sigma.renderers.canvas,
-        },
-        settings: {
-            defaultEdgeType: "arrow",
-            minArrowSize: 5,
-            sideMargin: 5,
-            defaultNodeColor: '#757575',
-            defaultLabelColor: '#757575',
-            defaultLabelSize: 15,
-            defaultLabelBGColor: '#ddd',
-            defaultLabelHoverColor: '#000',
-        }
-    }); 
+    let graphNodes = [];
+    let graphEdges = [];
 
     // Create graph nodes from packages
     for (let i = 0; i < packages.length; i++) {
-        s.graph.addNode({
-            id: packages[i].Name,
-            label: packages[i].Name,
-            y: 0,
-            x: 0+packages[i].Level,
-            size: 1
-        })        
+        graphNodes[i] =  {id: packages[i].Name, label: packages[i].Name, y:0, x:0+packages[i].Level, size:1};
     }
-  
+
     // Create graph edges from dependencies
     for (let i = 0; i < dependencies.length; i++) {
-        s.graph.addEdge({
-            id: 'edge_'+i,
-            source: dependencies[i].Parent,
-            target: dependencies[i].Name
-        });
+        graphEdges[i] =  {id: 'edge_'+i, source: dependencies[i].Parent, target: dependencies[i].Name, color:"#333"};
     }
 
     // MY Graph layout algorithm for arranging nodes to their proper level
     // Positioning nodes vertically
     var changes = true;
     let changelog = [];
-    
+
     let num = 0;
     while(changes){
         changes=false;
         let currentlog = [];
-        
-        for(let edge of s.graph.edges()) {
+
+        for(let edge of graphEdges) {
             let source;
             let target;
-            for(let node of s.graph.nodes()){
+            for(let node of graphNodes){
 
                 if(edge.source == node.label){
                     source = node;
                 }else if(edge.target == node.label){
-                    target = node;  
-                }  
-                if(typeof source == "object" && typeof target == "object" && target.label!=s.graph.nodes()[0].label){
-                    
+                    target = node;
+                }
+                if(typeof source == "object" && typeof target == "object" && target.label!=graphNodes[0].label){
+
                     if(source.x == target.x){
                         target.x+=1;;
                         currentlog.push(source.label);
                         currentlog.push(target.label);
-                        changes=true;    
+                        changes=true;
                     }
                     else if(target.x<source.x){
                         currentlog.push(source.label);
@@ -81,7 +56,7 @@ function calculateGraphData(packages, dependencies, drawGraph = true){
                         target.x = source.x+1;
                         changes=true;
                     }
-                    edge.color = colors[source.x];                       
+                    edge.color = colors[source.x];
                     break;
                 }
             }
@@ -103,14 +78,14 @@ function calculateGraphData(packages, dependencies, drawGraph = true){
         }
         num++;
     }
-    
+
 
     // Positioning nodes horizontally
-    let nodes = s.graph.nodes().sort((a,b) => (a.x > b.x) ? 1 : ((b.x > a.x) ? -1 : 0))
-    let edges = s.graph.edges();
+    let nodes = graphNodes.sort((a,b) => (a.x > b.x) ? 1 : ((b.x > a.x) ? -1 : 0))
+    let edges = graphEdges;
     let nArray = [];
     for(let i = 0; i<nodes.length; i++){
-        
+
         if(i<nodes.length-1 && nodes[i].x == nodes[i+1].x){
             nArray.push(nodes[i]);
         }else if(nArray.length>0){
@@ -121,7 +96,7 @@ function calculateGraphData(packages, dependencies, drawGraph = true){
             }else{
                 offset = nArray.length/6/nArray.length;
             }
-            
+
             for(let j=0; j<nArray.length; j++){
                 if(j==0){
                     nArray[j].y = Math.random() * (0.5 - -0.5) -0.5;
@@ -134,20 +109,48 @@ function calculateGraphData(packages, dependencies, drawGraph = true){
                         }else{
                             nArray[j].y = nArray[j-2].y-offset;
                         }
-                    }  
-                }             
+                    }
+                }
             }
             nArray=[];
-        }    
+        }
     }
 
-    if(drawGraph){
-        s.refresh();
-    }else{
-        s.graph.clear();
+    if(shouldDrawGraph){
+        drawGraph(graphNodes, graphEdges);
     }
 
     return [nodes, edges];
+}
+
+function drawGraph(graphNodes, graphEdges){
+    var s = new sigma({
+        container: 'container',
+        renderer: {
+            container: document.getElementById('container'),
+            type: sigma.renderers.canvas,
+        },
+        settings: {
+            defaultEdgeType: "arrow",
+            minArrowSize: 5,
+            sideMargin: 5,
+            defaultNodeColor: '#757575',
+            defaultLabelColor: '#757575',
+            defaultLabelSize: 15,
+            defaultLabelBGColor: '#ddd',
+            defaultLabelHoverColor: '#000',
+        }
+    });
+
+    for(let node of graphNodes){
+        s.graph.addNode(node);
+    }
+
+    for(let edge of graphEdges){
+        s.graph.addEdge(edge);
+    }
+
+    s.refresh();
 }
 
 export default calculateGraphData;
